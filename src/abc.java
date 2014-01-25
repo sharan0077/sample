efa7c5e74f4a35ed9bd560f66496af0cc5bd323d
Guru Sharan <sharan0077@gmail.com>
2014-01-25 16:02:38 +0530

    test written

---
+++ src/abc.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gurushan on 1/25/14.
+ */
+public class abc {
+    public  void sample(){
+        System.out.println("hi how are you");
+    }
+}
---
+++ test/abcTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gurushan on 1/25/14.
+ */
+public class abcTest {
+    public void sample_test()throws Exception{
+        System.out.println("ran test");
+    }
+}
